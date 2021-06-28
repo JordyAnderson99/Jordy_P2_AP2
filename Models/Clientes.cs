9b935835a27e3284f69066a625ab87c766599091
Jordy Anderson Celestino Mendoza <Jordy.Ander@hotmail.com>
2021-06-28 00:31:12 -0400

    Add project files.

---
+++ Models/Clientes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jordy_P2_AP2.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+    }
+}
---
+++ Models/Ventas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jordy_P2_AP2.Models
+{
+    public class Ventas
+    {
+        [Key]
+        public int VentaId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public int ClienteId { get; set; }
+        public double Monto { get; set; }
+        public double Balance { get; set; }
+    }
+}
