a706f3dae22dbc98238967008043c46f31f7081f
Jordy Anderson Celestino Mendoza <Jordy.Ander@hotmail.com>
2021-06-30 19:22:56 -0400

    Agregue la BLL de Cobros y Clientes

---
+++ BLL/ClientesBLL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Jordy_P2_AP2.Dal;
+using Jordy_P2_AP2.Models;
+
+namespace Jordy_P2_AP2.BLL
+{
+    public class ClientesBLL
+    {
+        public static List<Clientes> GetList(Expression<Func<Clientes, bool>> cliente)
+        {
+            List<Clientes> Lista = new List<Clientes>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                Lista = contexto.Clientes.Where(cliente).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
---
+++ BLL/CobrosBLL.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Jordy_P2_AP2.Dal;
+using Jordy_P2_AP2.Models;
+
+namespace Jordy_P2_AP2.BLL
+{
+    public class CobrosBLL
+    {
+        public static bool Guardar(Cobros cobro)
+        {
+            return Insertar(cobro);
+        }
+
+        
+        public static bool Eliminar(int id)
+        {
+            bool Eliminado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var cobro = Buscar(id);
+
+                contexto.Entry(cobro).State = EntityState.Deleted;
+                Eliminado = (contexto.SaveChanges() > 0);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Eliminado;
+        }
+
+        private static bool Insertar(Cobros cobro)
+        {
+            bool Insertado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                foreach (var item in cobro.Detalle)
+                {
+                    item.Venta = contexto.Ventas.Find(item.VentaId);
+                    item.Venta.Balance -= item.Cobrado;
+                    contexto.Entry(item.Venta).State = EntityState.Modified;
+                }
+                contexto.Cobros.Add(cobro);
+                Insertado = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Insertado;
+        }
+
+        public static Cobros Buscar(int id)
+        {
+            Cobros cobro = new Cobros();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                cobro = contexto.Cobros
+                    .Where(e => e.CobroId == id)
+                    .Include(e => e.Detalle)
+                    .FirstOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return cobro;
+        }
+
+        public static List<Cobros> GetList(Expression<Func<Cobros, bool>> cobro)
+        {
+            List<Cobros> Lista = new List<Cobros>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                Lista = contexto.Cobros.Where(cobro).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+    }
+}
